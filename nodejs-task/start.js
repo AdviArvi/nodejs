c58d38500016df3e2d5fc36049da53dba6ded9da
Shital <shital@Shital-PC.local>
2020-03-11 15:30:08 +0530

    task

--- nodejs-task/start.js
+++ nodejs-task/start.js
@@ -8,7 +8,13 @@ process.env.PORT=7700
 // const app = require('./app');
 app.set('port', process.env.PORT);
 app.get('/',function(req,res){
-  res.send(process.env.PORT);
+  const env={
+    user:"Shital",
+    pass:"sk",
+    port:7700
+  }
+  res.send("user:"+ env.user+" "+"pass:"+env.pass+" "+"port:"+env.port);
+  
   })
 const server = app.listen(app.get('port'), () => {
  
