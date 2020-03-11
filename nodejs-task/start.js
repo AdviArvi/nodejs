0640c47d0628a8b067ebf7a70bc8ab5f1f1332ed
Shital <shital@Shital-PC.local>
2020-03-11 14:37:47 +0530

    task

--- nodejs-task/start.js
+++ nodejs-task/start.js
@@ -1,1 +1,17 @@
-console.log("works");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+process.env.PORT=7700
+
+// const app = require('./app');
+app.set('port', process.env.PORT);
+app.get('/',function(req,res){
+  res.send(process.env.PORT);
+  })
+const server = app.listen(app.get('port'), () => {
+ 
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
